d9c384bc46072d86d9b666e67f85c826183a12df
ALLOMERDE <roberthorm@gmail.com>
2022-03-01 11:11:09 +0100

    Update skill.c

--- src/map/skill.c
+++ src/map/skill.c
@@ -12181,8 +12181,8 @@ static int skill_castend_pos2(struct block_list *src, int x, int y, uint16 skill
 		}
 
 		case PF_FOGWALL:
-		if( map->getcell(src->m, x, y, CELL_CHKLANDPROTECTOR) ) {
-			clif->skill_fail(sd,skill_id,USESKILL_FAIL_LEVEL,0);
+		if( map->getcell(src->m, src, x, y, CELL_CHKLANDPROTECTOR) ) {
+			clif->skill_fail(sd, skill_id, USESKILL_FAIL_LEVEL, 0, 0);
 			break;
 		}
 
@@ -12306,8 +12306,8 @@ static int skill_castend_pos2(struct block_list *src, int x, int y, uint16 skill
 			skill->unitsetting(src,skill_id,skill_lv,x,y,0);
 			break;
 		case WZ_ICEWALL:
-				if( map->getcell(src->m, x, y, CELL_CHKLANDPROTECTOR) ) {
-					clif->skill_fail(sd,skill_id,USESKILL_FAIL_LEVEL,0);
+				if( map->getcell(src->m, src, x, y, CELL_CHKLANDPROTECTOR) ) {
+					clif->skill_fail(sd,skill_id, USESKILL_FAIL_LEVEL, 0, 0);
 					break;
 				}
 
@@ -14862,7 +14862,7 @@ static int skill_unit_onleft(uint16 skill_id, struct block_list *bl, int64 tick)
 				sce->timer = timer->add(tick+skill->get_time2(skill_id,1), status->change_timer, bl->id, type);
 			}
 			break;
-		case PF_FOGWALL:
+		WALL:
 			if (sce) {
 				status_change_end(bl, type, INVALID_TIMER);
 				nullpo_retb(sc);
@@ -18201,14 +18201,14 @@ static int skill_cell_overlap(struct block_list *bl, va_list ap)
 		case HW_GANBANTEIN:
 					switch (unit->group->skill_id) {
 					case WZ_METEOR:
-						if(map->getcell(bl->m, bl->x, bl->y, CELL_CHKLANDPROTECTOR)){
+						if(map->getcell(bl->m, bl, bl->x, bl->y, CELL_CHKLANDPROTECTOR)){
 							return 1;
 						}else{
 							skill_delunit(unit);
 							return 1;
 						}
 					case WZ_STORMGUST:
-						if(map->getcell(bl->m, bl->x, bl->y, CELL_CHKLANDPROTECTOR)){
+						if(map->getcell(bl->m, bl, bl->x, bl->y, CELL_CHKLANDPROTECTOR)){
 							return 1;
 						}else{
 							skill_delunit(unit);
